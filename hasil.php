b94a04097073a4a9e0b07c4b1781cc3d3054e11b
cekaaaa <chelsykwan24@gmail.com>
2022-12-10 23:21:12 +0700

    home and result diagnosis

---
+++ hasil.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'framework/bootstrap.php'
+?>
+<body>
+<?php include 'navbar.php' ?>
+<link rel="stylesheet" href="css/hasil.css">
+<div class="container">
+    <h5 class="hasil text-center">Hasil Diagnosa</h5>
+    <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+                <div class="carousel-item top-50 active" data-bs-interval="10000">
+                    <div class="card m-auto" style="width: 50rem; height: 35rem;">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">Nama Penyakit</h5>
+                            <p class="card-text text-center">Keterangan penyakit</p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Nilai</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="carousel-item top-50" data-bs-interval="10000">
+                    <div class="card m-auto" style="width: 50rem; height: 35rem;">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">Nama Penyakit</h5>
+                            <p class="card-text text-center">Keterangan penyakit</p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Nilai</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="carousel-item top-50" data-bs-interval="10000">
+                    <div class="card m-auto" style="width: 50rem; height: 35rem;">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">Nama Penyakit</h5>
+                            <p class="card-text text-center">Keterangan penyakit</p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Nilai</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+    </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'framework/bootstrap.php'
+?>
+<body>
+<?php include 'navbar.php' ?>
+<link rel="stylesheet" href="css/index.css">
+<div class="container">
+    <div class=" d-flex card position-absolute top-50 start-50 translate-middle">
+      <div class="card-body">
+        <h5 class="card-title text-center">Expert System <br> Nutrition Condition Detection</h5>
+      </div>
+    </div>
+    <div class="d-flex justify-content-center btn-start">
+      <button type="button" class="btn">Start</button>
+    </div>
+</div>
+
+</body>
+</html>
